3809d22f969c1eca98c3822779622ff98844db30
ArchLeaders <archparliament@gmail.com>
2023-06-13 11:41:55 -0700

    Impl generic extension interfaces

---
+++ ExtensionManager.Core/Generics/IDataService.cs
@@ -0,0 +1,20 @@
+﻿using ExtensionManager.Core.Models;
+
+namespace ExtensionManager.Core.Generics;
+
+public interface IDataService : IServiceModule
+{
+    /// <summary>
+    /// Converts processed data to raw data and returns the result
+    /// </summary>
+    /// <param name="handle"></param>
+    /// <returns></returns>
+    public IFileHandle Process(IFileHandle handle);
+
+    /// <summary>
+    /// Reprocesses raw data and returns the result
+    /// </summary>
+    /// <param name="handle"></param>
+    /// <returns></returns>
+    public IFileHandle Reprocess(IFileHandle handle);
+}
---
+++ ExtensionManager.Core/Generics/IFormatService.cs
@@ -0,0 +1,18 @@
+﻿using ExtensionManager.Core.Models;
+
+namespace ExtensionManager.Core.Generics;
+
+public interface IFormatService : IServiceModule
+{
+    /// <summary>
+    /// Reads the <paramref name="handle"/> into the <see cref="IFormatService"/>
+    /// </summary>
+    /// <param name="handle"></param>
+    public void Read(IFileHandle handle);
+
+    /// <summary>
+    /// Writes the <see cref="IFormatService"/> payload to an <see cref="IFileHandle"/> and returns the result
+    /// </summary>
+    /// <returns></returns>
+    public IFileHandle Write();
+}
